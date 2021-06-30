50ca8fb37a4f253097be750e59cfd66bc929ea28
Alessio Olivieri <Alessio.Olivieri@zodiakactive.com>
2021-06-30 16:28:04 +0200

    cloud config server

---
+++ config-server/src/test/java/ollyit/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ollyit.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
